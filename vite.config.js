14cfc08c7651636df5deb8ee729e506387fc535c
Laila Kelloniemi <111287999+laila-kelloniemi@users.noreply.github.com>
2023-06-12 17:57:21 +0200

    Update vite.config.js
    images not loading, trying to fix it

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,9 @@ import {defineConfig} from 'vite'
 
 export default defineConfig({
 	plugins: []
-
-    
+	build: {
+	    assetsDir: 'images', // Output image files to the "images" directory
+	  },
 })
 
 
